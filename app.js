1dfb0c6155666e33d97ed59128b86b83970ef327
tnylee <tnyclee@gmail.com>
2022-03-14 22:47:17 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const words = require('./thai-wordlist.json')
+const messages = require('./results-20220314-215101.json')
+
+let commons = []
+
+for (const word of words) {
+    let count = 0
+
+    if (word.length > 3) {
+        for (const m of messages) {
+            if (m.message.includes('อยู่ไหน')) {
+                count += 1
+            }
+        }
+    
+        if (count > 0) {
+            commons.push({
+                word: 'อยู่ไหน',
+                count
+            })
+        }
+    }
+}
+
+commons.sort((a, b) => b.count - a.count)
+console.log(commons)
